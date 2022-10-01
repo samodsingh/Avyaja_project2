1f77681bdec5619a486e299c729be45d0e73428b
samod singh <samod0098@gmail.com>
2022-10-01 15:15:52 +0530

    Updates

---
+++ precache-manifest.39ab291c18ab2eb9d57064151e9be297.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "aabf587d30d13603dae9",
+    "url": "/Deployed_gh-pagas/static/css/main.cf749585.chunk.css"
+  },
+  {
+    "revision": "aabf587d30d13603dae9",
+    "url": "/Deployed_gh-pagas/static/js/main.aabf587d.chunk.js"
+  },
+  {
+    "revision": "991710d60cc142e1f491",
+    "url": "/Deployed_gh-pagas/static/js/1.991710d6.chunk.js"
+  },
+  {
+    "revision": "a1152c1264629d6cfab3",
+    "url": "/Deployed_gh-pagas/static/js/2.a1152c12.chunk.js"
+  },
+  {
+    "revision": "d717069cff32f9a0594e",
+    "url": "/Deployed_gh-pagas/static/js/runtime~main.d717069c.js"
+  },
+  {
+    "revision": "61b6ae6586aae72a7e53884f4fcb066a",
+    "url": "/Deployed_gh-pagas/static/media/logo.61b6ae65.png"
+  },
+  {
+    "revision": "ce42821bbca2e8a09a7d8a702659710e",
+    "url": "/Deployed_gh-pagas/static/media/imgplaystore.ce42821b.png"
+  },
+  {
+    "revision": "448d2fdd4f8c537bd53b8efe40db3509",
+    "url": "/Deployed_gh-pagas/static/media/about_feat_bg.448d2fdd.jpg"
+  },
+  {
+    "revision": "596764003d7cfc361aed85fe1e6f2a88",
+    "url": "/Deployed_gh-pagas/static/media/about_bottom_img.59676400.jpg"
+  },
+  {
+    "revision": "de05bc848333e4619741dce8fb505c0b",
+    "url": "/Deployed_gh-pagas/static/media/app-banner.de05bc84.jpg"
+  },
+  {
+    "revision": "2c0e4cb97e9a4511c9a33468e49486ac",
+    "url": "/Deployed_gh-pagas/static/media/backgroundimg.2c0e4cb9.jpg"
+  },
+  {
+    "revision": "ab35ef23458db2370e9d40f8421465a9",
+    "url": "/Deployed_gh-pagas/static/media/counter_bg.ab35ef23.jpg"
+  },
+  {
+    "revision": "b26dfcb9a9bf59433168e7145cea80cf",
+    "url": "/Deployed_gh-pagas/index.html"
+  }
+];
